3f1fd28f3b27ad57349711469fe0c7e275537ecd
luisbalru <balderasruizluis@gmail.com>
2019-10-24 09:59:36 +0200

    Sigo con el 2

--- R/Ejercicios23-10/Graficos.R
+++ R/Ejercicios23-10/Graficos.R
@@ -4,3 +4,5 @@
 # EJERCICIO 1
 boxplot(iris$Sepal.Length,iris$Sepal.Width,iris$Petal.Length,iris$Petal.Width,names=c("Sepal Length","Sepal Width", "Petal Length", "Petal Width"), ylab = "cm")
 
+# EJERCICIO 2
+M = matrix(runif(800),nrow=200,ncol=4)
