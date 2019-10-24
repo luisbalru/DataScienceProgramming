d2040279eba14dc006951ae05c20f8c10e6fb7bc
luisbalru <balderasruizluis@gmail.com>
2019-10-24 09:50:10 +0200

    Terminado ejercicio 1

--- R/Ejercicios23-10/Graficos.R
+++ R/Ejercicios23-10/Graficos.R
@@ -1,3 +1,6 @@
 # Ejercicios de Gráficos
 # Autor: Luis Balderas Ruiz
 
+# EJERCICIO 1
+boxplot(iris$Sepal.Length,iris$Sepal.Width,iris$Petal.Length,iris$Petal.Width,names=c("Sepal Length","Sepal Width", "Petal Length", "Petal Width"), ylab = "cm")
+
