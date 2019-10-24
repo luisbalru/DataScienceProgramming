a686be6bed401744a54757bdddda79805ecbf8b2
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2019-10-24 10:49:37 +0200

    Terminado ejercicio 3

--- R/Ejercicios23-10/Graficos.R
+++ R/Ejercicios23-10/Graficos.R
@@ -6,3 +6,11 @@ boxplot(iris$Sepal.Length,iris$Sepal.Width,iris$Petal.Length,iris$Petal.Width,na
 
 # EJERCICIO 2
 M = matrix(runif(800),nrow=200,ncol=4)
+
+# EJERCICIO 3
+library(MASS); 
+str(quine); 
+t=xtabs(~ Age,data=quine);
+p=prop.table(xtabs(~ Age,data=quine))
+union = cbind(t,p)
+barplot(union, beside=T)
