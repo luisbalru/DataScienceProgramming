1d1ced703244f69514e49aad333e8094ab540d27
luisbalru <balderasruizluis@gmail.com>
2019-11-04 13:29:34 +0100

    Subiendo plantilla. Voy por el 8

---
+++ Python/relacion-listas.py
@@ -0,0 +1,58 @@
+# Introducción a la programación en Ciencia de Datos
+# Relación de ejercicios sobre listas
+# Autor: Luis Balderas Ruiz
+
+def sum_nums_lista(numeros):
+
+
+print("1. Escribe una función sum_nums_lista(numeros) que sume todos los números de una lista. Compara el tiempo entre usar o no range")
+
+
+input("Pulsa enter para seguir")
+
+def contar_numeros_impares(numeros):
+
+
+print("2. Escribe una función contar_numeros_impares(numeros) que cuente la cantidad de número impares que hay en una lista.")
+
+input("Pulsa enter para seguir")
+
+def numeros_pares(numeros):
+
+
+print("3. Escribe una función numeros_pares(numeros) que devuelva los números pares que hay en una lista.")
+
+
+input("Pulsa enter para seguir")
+
+def combinar_listas(l1,l2):
+
+
+print("4. Escribe una función combinar_listas(l1, l2) que devuelva una lista que esté formada por todos los elementos de l1 y a continuación todos los de l2")
+
+input("Pulsa enter para seguir")
+
+
+def mezclar(la,lb):
+
+print("5. Escribe una función mezclar(la, lb) que dadas dos listas ordenadas devuelva una lista conteniendo los elementos de ambas listas ordenados de forma ascendente.")
+
+input("Pulsa enter para seguir")
+
+def traspuesta(matriz):
+
+print("6. La traspuesta de una matriz se obtiene intercambiado filas y columna. Escribe una función que devuelva la traspuesta de una matriz.")
+
+input("Pulsa enter para seguir")
+
+def contar_letras(palabra):
+
+print("7. Escribe una función contar_letras(palabra) que tome una palabra como argumento y devuelva una lista de pares en la que aparece cada letra junto con el número de veces que aparece esa letra en la palabra.")
+
+input("Pulsa enter para seguir")
+
+def eliminar(l1,l2):
+
+print("8. Escribe una función eliminar(l1, l2) que dadas dos listas devuelva una lista en la que estén todos los elementos de l1 que no están en l2.")
+
+input("Pulsa enter para seguir")
